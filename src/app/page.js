a1c2e60d90e4a51b5e593abde7ffda7228127405
Jayanth Chennupati <40850732+jayanth122@users.noreply.github.com>
2024-04-14 12:44:54 -0400

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
-      <GoogleAnalytics gaId="G-1D31K13119" />
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
